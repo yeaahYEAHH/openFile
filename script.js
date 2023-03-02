c13e8e5509e96e8cffefa7d937e8224857e46114
yeaahYEAHH <81806907+yeaahYEAHH@users.noreply.github.com>
2023-03-02 15:55:01 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,33 @@
-'use strict';
 const path = require('path'),
   fileSys = require('fs');
 
 document.addEventListener('DOMContentLoaded', () =>{
-    let btn = document.querySelector('.btn'),
-      fileList = document.querySelectorAll('.file'),
-      checkList = document.querySelectorAll('#check');
+  let btn = document.querySelector('.btn');
 
-    let move = function(files, check) {
-
-      for(let i = 0; i < files.length; i++){
-        let fileName =  path.win32.basename(fileList[i].value),
-        incorrectPath = path.resolve(fileName),
-        correctPath = incorrectPath.replaceAll('\\', '/');
-        if(check[i].checked){
-          fileSys.rename(correctPath, `C:/Users/Да это я/Documents/Открытие файлов/test/${fileName}`, err => {
-                if (err) throw err;
-                  console.log('Файл успешно перемещён');
-          })
+  let move = function(files, check, way) {
+    for(let i = 0; i < files.length; i++){    
+      if(check[i].checked){
+        let fileName =  path.win32.basename(files[i].value), correctPath;
+        for (const f of files[i].files) {
+          correctPath = f.path
         }
-      }    
-    }
+        fileSys.copyFile(correctPath, `${way}/${fileName}`, function (err) {
+          if (err) throw err;
+          console.log('File uploaded and moved!');
+        });
+        
+      }
+    }    
+  }
 
-    btn.addEventListener('click', () => {
-      let agga = path.resolve('Documents','caerthaedrfh.txt')
-      alert(agga.replaceAll('\\', '/'))
-      alert(fileList[0].value)
-      alert(path.resolve('./caerthaedrfh.txt'))
-      fileSys.rename(fileList[0].value, `C:/Users/Да это я/Documents/Открытие файлов/test/lox.txt`, err => {
-        if (err) throw err;
-          console.log('Файл успешно перемещён');
+  btn.addEventListener('click', () => {
+    let fileList = document.querySelectorAll('.file'),
+    checkList = document.querySelectorAll('#check'),
+    absolutpath = document.querySelector('#path').value.replaceAll('\\', '/');
+    console.log(path.resolve(__filename))
+    move(fileList, checkList, absolutpath)
   })
-      // file.forEach(item, () => {
-      //   let correctPath = move(item)
-      //   fileSys.rename(correctPath, way, err => {
-      //     if (err) throw err;
-      //       console.log('Файл успешно перемещён');
-      //     })
-      // })
-      
-      
-    })
 })
+
 
 
